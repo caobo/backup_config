05c468625c246fe95a39f3b01555eee3d70daa54
caobo <caobo@users.noreply.github.com>
2016-09-22 19:55:48 +0900

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,14 @@
+set fish_greeting ''
+
+set -x HOMEBREW_CASK_OPTS '--caskroom=/opt/homebrew-cask/Caskroom'
+#set -x PATH "/usr/local/bin" $PATH
+
+function printtime --on-event fish_postexec
+    if test $CMD_DURATION
+        set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
+        set_color green
+        echo $duration
+    end
+end
+
+alias v nvim
