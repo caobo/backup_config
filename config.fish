b6b7eb294fd4e3820fab063545c3b44744d742a9
caobo <caobo@users.noreply.github.com>
2017-01-16 18:16:00 +0900

    update python $PATH

--- config.fish
+++ config.fish
@@ -3,6 +3,8 @@ set fish_greeting ''
 set -x HOMEBREW_CASK_OPTS '--caskroom=/opt/homebrew-cask/Caskroom'
 #set -x PATH "/usr/local/bin" $PATH
 
+set -gx PATH "/usr/local/Cellar/python3" $PATH
+
 function printtime --on-event fish_postexec
     if test $CMD_DURATION
         set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
