6f57eb49d8991df84b404d183d53bda9f5b3206e
caobo <caobo@users.noreply.github.com>
2021-03-10 14:07:40 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,29 +1,30 @@
-set fish_greeting ''
+# set fish_greeting ''
 
-set -gx PATH "/usr/local/Cellar/python3" $PATH
-
-
+set -gx PATH /usr/local/bin/python3 $PATH
 
 # Change the prompt text
-set pure_symbol_prompt "~>"
-set pure_symbol_git_down_arrow "v"
-set pure_symbol_git_up_arrow "^"
-set pure_symbol_git_dirty "!"
-set pure_symbol_horizontal_bar "_"
+# set pure_symbol_prompt "❯"
+# set pure_symbol_git_down_arrow "v"
+# set pure_symbol_git_up_arrow "^"
+# set pure_symbol_git_dirty "!"
+# set pure_symbol_horizontal_bar "_"
+# pure_enable_git true
 
 # Change the colors
 # set pure_color_blue (set_color "1e00fd")
 # set pure_color_cyan (set_color "1e95fd")
 # set pure_color_gray (set_color "6c6c6c")
 # set pure_color_green (set_color "66ff66")
-# set pure_color_normal (set_color "000000")
+# set pure_color_normal (set_color "1e00fd")
 # set pure_color_red (set_color "f820ff")
+# set pure_color_red (set_color "66ff66")
 # set pure_color_yellow (set_color "1bc8c8")
 
 # Change colors for username and host in SSH
 # set pure_username_color $pure_color_yellow
 # set pure_host_color $pure_color_green
-# set pure_root_color $pure_color_red
+# set pure_root_color $pure_color_green
+set pure_color_success 6638F0
 
 # Change where the username and host is displayed
 # 0 - end of prompt, default
